f14786d81c244a00603deb0913e85820dd7f067a
Christian <Christian@VAIO>
2014-11-23 16:29:25 +0000

    Event class data types, constructors and appropriate doc comments.

--- src/comp2541/coursework/cwk2/Event.java
+++ src/comp2541/coursework/cwk2/Event.java
@@ -1,5 +1,39 @@
 package comp2541.coursework.cwk2;
 
+import org.joda.money.Money;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+/**
+ * @author Christian J. Gibbs
+ * COMP2451 Coursework 2
+ *
+ */
+
 public class Event
 {
+	private String artists;
+	private LocalDate date;
+	private LocalTime doors;
+	private Money ticketPrice;
+	private int ticketsSold;
+	
+	/**
+	 * This is the main constructor for this Event class
+	 * @param artists
+	 * @param date
+	 * @param doors
+	 * @param ticketPrice
+	 * @param ticketsSold
+	 */
+	
+	public Event(String artists, LocalDate date, LocalTime doors,
+			Money ticketPrice, int ticketsSold) {
+		super();
+		this.artists = artists;
+		this.date = date;
+		this.doors = doors;
+		this.ticketPrice = ticketPrice;
+		this.ticketsSold = ticketsSold;
+	}	
 }
